197e3d3cf7e75490695c96f836ef1d74673e5383
Pete Doherty <ethagnawl@gmail.com>
2012-07-20 22:57:20 -0400

    pulling instagram creds from config file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,17 @@
+if defined?(INSTAGRAM_CONFIG)
+  #nothing to do
+elsif (!ENV["BUCKET_NAME"].nil? && !ENV["BUCKET"].nil? && !ENV["ACCESS_KEY_ID"].nil? && !ENV["SECRET_ACCESS_KEY"].nil?)
+  S3_CONFIG = {
+    "bucket" => ENV["BUCKET"],
+    "access_key_id" => ENV["ACCESS_KEY_ID"],
+    "secret_access_key" => ENV["SECRET_ACCESS_KEY"]}
+else
+  require 'yaml'
+  hash = File.open("#{Rails.root}/config/instagram.yml") do |f| YAML::load(f) end
+  INSTAGRAM_CONFIG = hash[Rails.env.to_s]
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, 'f7c32457a6f8489d8ea81322b9d7c889', 'c845602a26f749fa814e24c8ccdadbbe'
+  provider :instagram, INSTAGRAM_CONFIG['id'], INSTAGRAM_CONFIG['secret']
 end
 
