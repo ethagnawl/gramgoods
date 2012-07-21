7e0503da43d47a130442af5c97d9e5f8164144e9
Pete Doherty <ethagnawl@gmail.com>
2012-07-21 18:12:14 -0400

    adding status to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :store, :dependent => :destroy
 
-  attr_accessible :name, :price, :quantity, :description, :store_id
+  attr_accessible :name, :price, :quantity, :description, :store_id, :status
   validates_presence_of :name, :price, :quantity, :description
 end
