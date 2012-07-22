7291a119b9c3854762112c5dda7ed7bdec106477
Pete Doherty <ethagnawl@gmail.com>
2012-07-22 19:46:55 -0400

    removing old instagram code from ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-
-  #- configure_instagram(@user.authentications.first.uid, @user.authentications.first.access_token)
-  #= Instagram.user_recent_media(@user.authentications.first.uid)
-  #- Instagram.reset
-
   def configure_instagram(client_key, access_token)
     Instagram.configure do |config|
       config.client_id = client_key
