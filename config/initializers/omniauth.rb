4cfc03f7050eb2d710e08ba8f14c409167f51f6a
Pete Doherty <ethagnawl@gmail.com>
2012-07-22 01:21:15 -0400

    adding heroku instagram_config to omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 if defined?(INSTAGRAM_CONFIG)
   #nothing to do
-elsif (!ENV["BUCKET_NAME"].nil? && !ENV["BUCKET"].nil? && !ENV["ACCESS_KEY_ID"].nil? && !ENV["SECRET_ACCESS_KEY"].nil?)
-  S3_CONFIG = {
-    "bucket" => ENV["BUCKET"],
-    "access_key_id" => ENV["ACCESS_KEY_ID"],
-    "secret_access_key" => ENV["SECRET_ACCESS_KEY"]}
+elsif (!ENV["INSTAGRAM_ID"].nil? && !ENV["INSTAGRAM_SECRET"].nil?)
+  INSTAGRAM_CONFIG = {
+    'id' => ENV['INSTAGRAM_ID'],
+    'secret' => ENV['SECRET']
+  }
 else
   require 'yaml'
   hash = File.open("#{Rails.root}/config/instagram.yml") do |f| YAML::load(f) end
