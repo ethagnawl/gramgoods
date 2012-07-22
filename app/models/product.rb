153c3f16f1528ddbd074510c639f111e08db2d5e
Pete Doherty <ethagnawl@gmail.com>
2012-07-22 03:14:36 -0400

    adding new columns to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :store, :dependent => :destroy
 
-  attr_accessible :name, :price, :quantity, :description, :store_id, :status
-  validates_presence_of :name, :price, :quantity, :description
+  attr_accessible :name, :price, :quantity, :description, :store_id, :status,
+  :colors, :sizes, :flatrate_shipping_cost, :instagram_tag
+  validates_presence_of :name, :price, :quantity, :description, :instagram_tag
 end
