f7efc0092c524517ac8dadab3974c230885c9a33
Pete Doherty <ethagnawl@gmail.com>
2012-07-22 19:57:20 -0400

    adding gon to application controller and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :clear_gon
+  before_filter :set_gon
 
   def after_sign_in_path_for(resource)
     stores_path
+  end
+
+  private
+
+  def set_gon
+    gon.page = "#{params[:controller]}_#{params[:action]}"
+  end
+
+  def clear_gon
+    gon.clear
   end
 end
