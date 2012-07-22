585f1c1c42c8015189d8e6bc17d4a1330fcea613
Pete Doherty <ethagnawl@gmail.com>
2012-07-22 15:46:54 -0400

    sticky product images in products/_form

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :quantity, :description, :store_id, :status,
   :colors, :sizes, :flatrate_shipping_cost, :instagram_tag, :photos
   validates_presence_of :name, :price, :quantity, :description, :instagram_tag
+
+  def photos_array
+    return [] if self.photos.nil?
+    return [self.photos] unless self.photos.include?(',')
+    self.photos.split(',')
+  end
 end
