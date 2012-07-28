cb4cee709f0f2b3db8859f7e98fe785d20262aaf
Pete Doherty <ethagnawl@gmail.com>
2012-07-28 05:02:42 -0400

    adding product_photo js template to applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,22 +11,48 @@ class ApplicationController < ActionController::Base
     stores_path
   end
 
+  def _get_instagram_photo_feed_for_user
+    product = params[:product_slug] == '' ? nil : Product.find_by_slug(params[:product_slug])
+    render :json => {
+      :product_photos => view_context.get_instagram_photo_feed_for_user(current_user).map { |photo| render_user_photo_template(product, photo) }}
+  end
+
   private
+
+  def render_user_photo_template(product = nil, photo)
+    selected = product.nil? ? false : product.photos_array.include?(view_context.product_photo_url(photo))
+    {
+      :url => view_context.product_photo_url(photo),
+      :selected => selected ? 'selected' : nil,
+      :btnClass => selected ? 'btn-success' : 'btn-inverse'
+    }
+  end
 
   def render_product_widget_template(store, product)
     {
       :name => product.name,
       :instagram_tag => product.instagram_tag,
       :truncated_name => truncate(product.name, :length => 38),
+      :slug => product.slug,
+      :store_slug => store.slug,
+      :store_id => store.id,
       :url => "/stores/#{store.slug}/products/#{product.slug}",
       :description => truncate(product.description, :length => 45),
+      :raw_description => product.description,
       :price => number_to_currency(product.price),
+      :raw_price => product.price,
       :quantity => product.get_quantity,
+      :raw_quantity => product.quantity,
+      :unlimited_quantity => product.unlimited_quantity,
       :colors => product.colors ||= nil,
       :sizes => product.sizes ||= nil,
       :flatrate_shipping_cost => product.flatrate_shipping_cost.nil? ? nil : number_to_currency(product.flatrate_shipping_cost),
+      :raw_flatrate_shipping_cost => product.flatrate_shipping_cost.nil? ? nil : product.flatrate_shipping_cost,
       :status => product.status,
-      :product_count => "#{product.photos_array.length} #{(product.photos_array.length > 1 ? 'Photos' : 'Photo')}",
+      :draft => product.status == 'Draft',
+      :active => product.status == 'Active',
+      :out_of_stock => product.status == 'Out of Stock',
+      :product_count => "#{product.photos_array.length} #{(product.photos_array.length == 0 || product.photos_array.length > 1 ? 'Photos' : 'Photo')}",
       :product_photo => product.photos_array.first
     }
   end
