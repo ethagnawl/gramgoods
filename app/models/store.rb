8b5b1d3d780fd508826978d343b9bb77416fd614
Pete Doherty <ethagnawl@gmail.com>
2012-07-29 23:43:48 -0400

    adding store model validations

--- app/models/store.rb
+++ app/models/store.rb
@@ -7,5 +7,15 @@ class Store < ActiveRecord::Base
 
   attr_accessible :name, :url, :user_id, :return_policy
   validates_presence_of :name, :url, :return_policy
-  validates_uniqueness_of :url
+  validates_uniqueness_of :url, :name
+
+  validate :url_has_changed, :on => :update
+
+  private
+
+  def url_has_changed
+    if self.url_changed? && self.url != self.url_was
+      errors[:base] = 'You cannot change your store\'s URL.'
+    end
+  end
 end
