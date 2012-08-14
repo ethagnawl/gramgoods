d3e300c170268aa2ad53d79402daa63c42aa7c0b
Pete Doherty <ethagnawl@gmail.com>
2012-08-14 03:02:32 -0400

    orders now only have one line_item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,6 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
 
-  attr_accessible :quantity, :size, :color, :size, :price, :total
-  validates_presence_of :quantity, :size, :color, :size, :price, :total
+  attr_accessible :quantity, :color, :size, :price, :total, :flatrate_shipping_cost
+  validates_presence_of :quantity, :price, :total
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ActiveRecord::Base
   belongs_to :store
-  has_many :line_items, :order => 'updated_at DESC'
+  has_one :line_item, :order => 'updated_at DESC'
   has_one :recipient
 
-  attr_accessible :line_items_attributes, :recipient_attributes
+  attr_accessible :line_item_attributes, :recipient_attributes, :status
 
-  accepts_nested_attributes_for :line_items, :recipient
+  accepts_nested_attributes_for :line_item, :recipient
 end
