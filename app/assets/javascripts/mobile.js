01f72f970480c573676dd751a49e6df7a4a7eddd
Pete Doherty <ethagnawl@gmail.com>
2012-08-14 03:00:58 -0400

    adding happy form validation

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,4 +1,6 @@
 //= require ./mobile/zepto.min
+//= require ./mobile/happy_form
+//= require ./mobile/happy_form_methods
 //= require ./common/rails
 //= require ./common/mustache
 //= require_tree ./mobile
