ed010dc004c015df3f53601d13f3b5184598d24f
Pete Doherty <ethagnawl@gmail.com>
2012-08-15 01:23:32 -0400

    adding product_id to LineItem attr_accessible and validates_presence_of

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
 
-  attr_accessible :quantity, :color, :size, :price, :total, :flatrate_shipping_cost
-  validates_presence_of :quantity, :price, :total
+  attr_accessible :quantity, :color, :size, :price, :total, :flatrate_shipping_cost,
+    :product_id
+  validates_presence_of :quantity, :price, :total, :product_id
 end
