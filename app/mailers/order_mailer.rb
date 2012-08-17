6866fe1c287cdab76a74e0d71f35c29462b94399
Pete Doherty <ethagnawl@gmail.com>
2012-08-17 00:06:11 -0400

    adding cc and reply-to to OrderMailer#order_confirmation

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,12 @@
 class OrderMailer < ActionMailer::Base
-  default :from => "admin@gramgoods.com"
+  default :from => "admin@gramgoods.com",
+          :to => order.recipient.email_address,
+          :cc => [order.store.user.email, 'admin@gramgoods.com'],
+          :reply_to => 'admin@gramgoods.com'
 
   def order_confirmation(order, product)
     @order = order
     @product = product
-    mail(:to => order.recipient.email_address, :subject => 'GramGoods Order NNN')
+    mail(:subject => "GramGoods Order Confirmation - Order ##{order.id}")
   end
 end
