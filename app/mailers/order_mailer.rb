cd1929b019789fdd98963f59053b5a96d98164d7
Pete Doherty <ethagnawl@gmail.com>
2012-08-17 00:10:21 -0400

    fixing OrderMailer bug

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,12 +1,13 @@
 class OrderMailer < ActionMailer::Base
   default :from => "admin@gramgoods.com",
-          :to => order.recipient.email_address,
-          :cc => [order.store.user.email, 'admin@gramgoods.com'],
           :reply_to => 'admin@gramgoods.com'
 
   def order_confirmation(order, product)
     @order = order
     @product = product
-    mail(:subject => "GramGoods Order Confirmation - Order ##{order.id}")
+
+    mail( :to => order.recipient.email_address,
+          :cc => [order.store.user.email, 'admin@gramgoods.com'],
+          :subject => "GramGoods Order Confirmation - Order ##{order.id}")
   end
 end
