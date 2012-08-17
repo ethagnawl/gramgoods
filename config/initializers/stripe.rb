59cf6f92f8744f1d5561d55ebf32532e8751cefc
Pete Doherty <ethagnawl@gmail.com>
2012-08-17 18:02:16 -0400

    pulling stripe config from env vars - heroku now uses the GramGoods stripe test account

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,12 @@
-Stripe.api_key = " sk_09rx3kqLuNYeMQdBDQXenha6U7AOw "
-STRIPE_PUBLIC_KEY = "pk_09rxQXsXg8HHuKfNWKCZQCi4WuVH0"
+if (!ENV["STRIPE_API_KEY"].nil? && !ENV["STRIPE_PUBLIC_KEY"].nil?)
+  STRIPE_CONFIG = {
+    "API_KEY" => ENV["STRIPE_API_KEY"],
+    "PUBLIC_KEY" => ENV["STRIPE_PUBLIC_KEY"]
+  }
+else
+  require 'yaml'
+  hash = File.open("config/stripe_config.yml") do |f| YAML::load(f) end
+  STRIPE_CONFIG = hash[Rails.env.to_s]
+end
 
-
+Stripe.api_key = STRIPE_CONFIG['API_KEY']
