d3a622dfecb52c7f4ff529cb67ccb159377a31ec
Pete Doherty <ethagnawl@gmail.com>
2012-08-17 16:24:02 -0400

    removing hardcoded localhost:3000 from create_order_url

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -86,7 +86,7 @@ class ProductsController < ApplicationController
     if Rails.env.production?
       gon.create_order_url = "#{ENV['SECURE_HEROKU_APP_URL']}/stores/#{@store.slug}/orders/new"
     else
-      gon.create_order_url = "http://localhost:3000/stores/#{@store.slug}/orders/new"
+      gon.create_order_url = "/stores/#{@store.slug}/orders/new"
     end
 
     respond_to do |format|
