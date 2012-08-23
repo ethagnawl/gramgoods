68880e53f46b4b39421a71f8f915032519a32b10
Pete Doherty <ethagnawl@gmail.com>
2012-08-22 23:10:59 -0400

    adding action_mailer.default_url_options and assoc value

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Gramgoods::Application.configure do
   config.to_prepare { Devise::SessionsController.force_ssl }
   config.to_prepare { Devise::RegistrationsController.force_ssl }
   config.to_prepare { Devise::PasswordsController.force_ssl }
+
+  config.action_mailer.default_url_options = { :host => ENV['HEROKU_APP_URL'] }
 end
