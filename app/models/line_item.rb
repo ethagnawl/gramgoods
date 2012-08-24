4ad95434c88ff2e40a28fc4ea8780624d5415403
Pete Doherty <ethagnawl@gmail.com>
2012-08-24 13:47:54 -0400

    adding product_attributes validation to LineItem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -8,6 +8,26 @@ class LineItem < ActiveRecord::Base
 
   validate :line_item_quantity_must_be_less_than_or_equal_to_product_quantity
   validate :line_item_order_product_status_must_be_active
+  validate :product_attributes
+
+  def product_attributes
+    product = Product.find(self.product_id)
+
+    unless self.flatrate_shipping_cost.nil?
+      if product.flatrate_shipping_cost != self.flatrate_shipping_cost
+        errors.add(:flatrate_shipping_cost, 'must be valid.')
+      end
+    end
+    if product.name != self.product_name
+      errors.add(:product_name, 'must be valid.')
+    end
+    if product.price != self.price
+      errors.add(:price, 'must be valid.')
+    end
+    if product.id != self.product_id
+      errors.add(:product_id, 'must be valid.')
+    end
+  end
 
   def line_item_quantity_must_be_less_than_or_equal_to_product_quantity
     unless self.product.quantity.nil?
