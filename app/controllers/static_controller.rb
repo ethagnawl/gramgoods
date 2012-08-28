d52f6ecef3bbee19e6aab054f877c9828942e4f2
Pete Doherty <ethagnawl@gmail.com>
2012-08-28 01:27:51 -0400

    redirect to recent products if mobile

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
   layout 'admin'
   def index
+    # TODO move this into a before_filter in ApplicationController
+    redirect_to(products_path) if mobile_device?
   end
 end
