6ab994f634d72f6a1bbc4a1553523f181217f339
Pete Doherty <ethagnawl@gmail.com>
2012-09-02 12:54:13 -0400

    adding link to desktop version to products partial

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ class StaticController < ApplicationController
   layout 'admin'
   def index
     # TODO move this into a before_filter in ApplicationController
-    redirect_to(products_path) if mobile_device?
+    redirect_to(products_path) if mobile_device? && params[:layout] != 'desktop'
   end
 end
