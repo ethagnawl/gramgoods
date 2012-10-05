7d63faa938300cf8945a8ce0d37bdebcee569b47
Pete Doherty <ethagnawl@gmail.com>
2012-10-04 21:58:32 -0400

    adding tos confirmation to store creation

--- app/models/store.rb
+++ app/models/store.rb
@@ -6,9 +6,10 @@ class Store < ActiveRecord::Base
 
   friendly_id :name, :use => [:slugged, :history]
 
-  attr_accessible :name, :return_policy
+  attr_accessible :name, :return_policy, :terms_of_service
   validates_presence_of :name, :return_policy
   validates_uniqueness_of :name
+  validates_acceptance_of :terms_of_service, :on => :create
 
   def displayable_products
     self.products.reject { |product| product.status == 'Draft' }
