db47ff23f1f57b7ecd504559e4be03f6c5daf6db
Pete Doherty <ethagnawl@gmail.com>
2012-10-08 23:39:23 -0400

    adding todo to after_login flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     unless current_user.first_store.nil?
       store = current_user.first_store
-      flash[:notice] = "#{store.name} has been created successfully."
+      flash[:notice] = "#{store.name} has been created successfully. - TODO: only show after store creation."
       new_store_product_path(store)
     else
       new_store_path
