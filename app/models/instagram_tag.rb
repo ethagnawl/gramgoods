089b209dcb9ff741e41a541603c3af10158c8cc5
Pete Doherty <ethagnawl@gmail.com>
2012-10-11 14:50:04 -0400

    stripping leading hashes from InstagramTag

--- app/models/instagram_tag.rb
+++ app/models/instagram_tag.rb
@@ -2,4 +2,11 @@ class InstagramTag < ActiveRecord::Base
   belongs_to :product
   attr_accessible :instagram_tag
   validates_presence_of :instagram_tag
+  before_save :strip_leading_hash_from_instagram_tag
+
+  private
+
+    def strip_leading_hash_from_instagram_tag
+      self.instagram_tag.gsub!(/^#+/, '')
+    end
 end
