c1ab835e825614ad0f9edf78924e58e75e386675
Pete Doherty <ethagnawl@gmail.com>
2012-10-12 14:37:06 -0400

    replacing whitespace in InstagramTag with before_save

--- app/models/instagram_tag.rb
+++ app/models/instagram_tag.rb
@@ -2,11 +2,16 @@ class InstagramTag < ActiveRecord::Base
   belongs_to :product
   attr_accessible :instagram_tag
   validates_presence_of :instagram_tag
-  before_save :strip_leading_hash_from_instagram_tag
+  before_save :strip_leading_hashes
+  before_save :replace_whitespace_with_underscores
 
   private
 
-    def strip_leading_hash_from_instagram_tag
+    def strip_leading_hashes
       self.instagram_tag.gsub!(/^#+/, '')
+    end
+
+    def replace_whitespace_with_underscores
+      self.instagram_tag.gsub!(/\s+/, '_')
     end
 end
