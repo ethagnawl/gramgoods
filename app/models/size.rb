d5364c88aa3ff0e428325447a80e1a56c885c3fb
Pete Doherty <ethagnawl@gmail.com>
2012-10-16 00:44:48 -0400

    adding before_validation callbacks to color and size

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,4 +1,26 @@
 class Size < ActiveRecord::Base
   belongs_to :product
   attr_accessible :size
+
+  before_validation :strip
+  before_validation :parse_csv_and_build_additional_objects
+
+  private
+  def strip
+    attribute = self.class.to_s.downcase
+    self[attribute] = self.send(attribute).strip
+  end
+
+  def parse_csv_and_build_additional_objects
+    attribute = self.class.to_s.downcase
+    return unless self.send(attribute) =~ /,+/
+    product = self.product
+    build_or_create = product.persisted? ? 'create' : 'build'
+    values = self.send(attribute).split(',')
+    self[attribute] = values.shift
+    values.each do |value|
+      (product.send(attribute.pluralize)).send(build_or_create,
+                                               :"#{attribute}" => value )
+    end
+  end
 end
