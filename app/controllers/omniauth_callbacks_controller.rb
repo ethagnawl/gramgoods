c17d787cf810e4449c5aa2f7ae17e2096b5745a8
Pete Doherty <ethagnawl@gmail.com>
2012-10-23 19:03:53 -0400

    preventing users from creating accounts without creating stores

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,13 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def instagram
     #raise request.env['omniauth.auth'].to_yaml
-    user_params = request.env['omniauth.params']['user']
-    store_params = request.env['omniauth.params']['store']
+    user_params = request.env['omniauth.params']['user'] ||= nil
+    store_params = request.env['omniauth.params']['store'] ||= nil
     user = User.from_omniauth(request.env['omniauth.auth'], user_params, store_params)
-    if user.persisted?
+    if !user
+      flash[:alert] = 'Please create an account before signing in.'
+      redirect_to products_path
+    elsif user.persisted?
       flash[:notice] = "Signed in successfully as #{user.username}."
       sign_in_and_redirect(user)
     else
