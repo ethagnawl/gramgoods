d8e5ca0e4ed5cd66001458e346f80dddf06c4114
Pete Doherty <ethagnawl@gmail.com>
2012-10-24 02:30:20 -0400

    removing ProductImage

--- app/models/product_image.rb
+++
@@ -1,7 +0,0 @@
-class ProductImage < ActiveRecord::Base
-  belongs_to :product
-
-  attr_accessible :instagram_id, :url, :tags, :thumbnail, :likes
-
-  validates_presence_of :instagram_id, :url, :likes, :thumbnail
-end
