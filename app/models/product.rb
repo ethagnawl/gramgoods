8484232a76f91c9f588e5464827b8b69915c0ece
Pete Doherty <ethagnawl@gmail.com>
2012-10-24 01:17:09 -0400

    removing product#product_image_urls

--- app/models/product.rb
+++ app/models/product.rb
@@ -93,10 +93,6 @@ class Product < ActiveRecord::Base
     end
   end
 
-  def product_image_urls
-    self.product_images.map { |product_image| product_image.url }
-  end
-
   def get_quantity
     self.unlimited_quantity == true ? 'Unlimited Quantity' : self.quantity
   end
