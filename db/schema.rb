6f181dcc22d1cce859beea3434ff71916b849ea4
Pete Doherty <ethagnawl@gmail.com>
2012-10-24 02:31:08 -0400

    removing token_authentication columns from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024052313) do
+ActiveRecord::Schema.define(:version => 20121024054724) do
 
   create_table "authentications", :force => true do |t|
     t.datetime "created_at",   :null => false
@@ -165,10 +165,8 @@ ActiveRecord::Schema.define(:version => 20121024052313) do
     t.string   "username"
     t.string   "thumbnail"
     t.string   "access_token"
-    t.string   "authentication_token"
   end
 
-  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
