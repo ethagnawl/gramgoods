2c421e84d71b45b4afe8832fdbd8ed9a7e3f64e4
Pete Doherty <ethagnawl@gmail.com>
2012-12-19 11:31:53 -0500

    removing add2home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require ./mobile/modernizr.min.js
 //= require ./mobile/happy_form
 //= require ./mobile/happy_form_methods
-//= require ./mobile/add2home_config
 //= require ./common/rails
 //= require ./common/mustache
 //= require_tree ./mobile
