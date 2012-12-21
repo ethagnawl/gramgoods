e1dcaf9a0b12011ad45b02a25fbc00978cf1b89d
Pete Doherty <ethagnawl@gmail.com>
2012-12-21 17:31:37 -0500

    using ADMIN_EMAIL_ADDRESS in order_mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,12 +1,12 @@
 class OrderMailer < ActionMailer::Base
-  default :from => "admin@gramgoods.com",
-          :reply_to => 'admin@gramgoods.com'
+  default :from => ADMIN_EMAIL_ADDRESS,
+          :reply_to => ADMIN_EMAIL_ADDRESS
 
   def order_confirmation(order)
     @order = order
 
     mail( :to => order.recipient.email_address,
-          :cc => [order.store.user.email, 'admin@gramgoods.com'],
+          :cc => [order.store.user.email, ADMIN_EMAIL_ADDRESS],
           :subject => "GramGoods Order Confirmation - Order ##{order.id}")
   end
 end
