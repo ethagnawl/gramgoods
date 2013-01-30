fa783b3d5bb9046d40e55d039e15f90ff7a68d69
Pete Doherty <ethagnawl@gmail.com>
2013-01-30 17:57:20 -0500

    no longer setting and re-setting AMAZON_CONFIG constant

--- config/initializers/01_amazon.rb
+++ config/initializers/01_amazon.rb
@@ -1,10 +1,10 @@
 Gramgoods::Application.configure do
-  AMAZON_CONFIG = {}
 
   if Rails.env.development?
     hash = File.open("#{Rails.root}/config/amazon_config.yml") do |f| YAML::load(f) end
     AMAZON_CONFIG = hash[Rails.env.to_s]
   else
+    AMAZON_CONFIG = {}
     AMAZON_CONFIG['AWS_BUCKET'] = ENV['AWS_BUCKET']
     AMAZON_CONFIG['AWS_ACCESS_KEY_ID'] = ENV['AWS_ACCESS_KEY_ID']
     AMAZON_CONFIG['AWS_SECRET_ACCESS_KEY'] = ENV['AWS_SECRET_ACCESS_KEY']
