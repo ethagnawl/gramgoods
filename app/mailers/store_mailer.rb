d1d9f327a4cbb7cedeb440fb144a9bbf3f57c988
Pete Doherty <ethagnawl@gmail.com>
2013-06-07 17:47:20 -0400

    using locals in shared/store_confirmation

--- app/mailers/store_mailer.rb
+++ app/mailers/store_mailer.rb
@@ -3,7 +3,6 @@ class StoreMailer < ActionMailer::Base
           :reply_to => ADMIN_EMAIL_ADDRESS
 
   def store_confirmation(store, owner)
-    pry
     @store = store
 
     mail( :to => owner.email_address,
